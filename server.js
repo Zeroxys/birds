2a5da0511d2f04454e1247ce7dc55d43c2680aed
zeroxys <miguelzavalac@gmail.com>
2016-11-22 13:35:05 -0600

    Entendiendo como crear funciones asincronas con process.nextTick

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var fs = require('fs')
+/*var fs = require('fs')
 
 var readFileText = function(name,callback){
 	process.nextTick(function(){
@@ -10,4 +10,21 @@ var readFileText = function(name,callback){
 readFileText('./package.json', function(content){
 	console.log(content)
 })
-console.log('hola mundo')
+console.log('hola mundo')*/
+
+
+var fs = require('fs')
+
+var readFileAsinc = function(name, callback){
+	process.nextTick(function(){
+		var content = fs.readFileSync(name)
+		console.log(content.toString())		
+	})
+}
+
+readFileAsinc('./package.json', function(content){
+	console.log(content)
+})
+
+console.log('hola platzi')
+console.log('/////////////////////////')
